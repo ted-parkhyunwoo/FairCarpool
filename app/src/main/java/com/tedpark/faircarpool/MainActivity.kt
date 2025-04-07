0f11489b3b77651bb9a8c8872f658422f0622222
ted <zoogree@gmail.com>
2025-04-07 22:59:08 +0900

    update comment

--- app/src/main/java/com/tedpark/faircarpool/MainActivity.kt
+++ app/src/main/java/com/tedpark/faircarpool/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.tedpark.faircarpool.ui.theme.FairCarpoolTheme
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
@@ -30,53 +31,99 @@ fun DriveDataUI() {
     var totalDistance by remember { mutableStateOf("") }
     var fuelEconomy by remember { mutableStateOf("") }
     var fuelCost by remember { mutableStateOf("") }
-    var tollFee by remember { mutableStateOf("0") }
-    var totalPerson by remember { mutableStateOf("1") }
+    var tollFee by remember { mutableStateOf("") }
+    var etcCost by remember { mutableStateOf("") }
+    var totalPerson by remember { mutableStateOf("") }
+    var result by remember { mutableStateOf("None") }
 
-    var result by remember { mutableStateOf("") }
-
+    // OutlinedTextField 필드를 생성하는 함수.
+    //원형: OutlinedTextField(value = totalDistance, onValueChange = { totalDistance = it }, modifier = Modifier.fillMaxWidth(), suffix = { Text("KM") }, label = { Text("총 운행거리", ) })
+    @Composable
+    fun InputField(
+        label: String,
+        value: String,
+        onValueChange: (String) -> Unit,
+        suffix: String = "",
+        modifier: Modifier = Modifier
+            .fillMaxWidth()
+            .height(63.dp),
+        validateBlank: Boolean = true
+    ) {
+        OutlinedTextField(
+            value = value,
+            onValueChange = onValueChange,
+            label = { Text(label, fontSize = 12.sp) },
+            isError = validateBlank && value.isBlank(), // 조건부 에러 적용
+            modifier = modifier,
+            suffix = { if (suffix.isNotEmpty()) Text(suffix) }
+        )
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
         verticalArrangement = Arrangement.Top
     ) {
-        Text("입력:")
-        OutlinedTextField(value = totalDistance, onValueChange = { totalDistance = it }, label = { Text("총 운행거리 (KM)") })
-        OutlinedTextField(value = fuelEconomy, onValueChange = { fuelEconomy = it }, label = { Text("연비 (Km/L)") })
-        OutlinedTextField(value = fuelCost, onValueChange = { fuelCost = it }, label = { Text("연료 가격 (원)") })
-        OutlinedTextField(value = tollFee, onValueChange = { tollFee = it }, label = { Text("톨게이트 비용 (원)") })
-        OutlinedTextField(value = totalPerson, onValueChange = { totalPerson = it }, label = { Text("탑승인원") })
+        Text("필수:", fontSize = 12.sp)
+        InputField("총 운행거리", totalDistance, { totalDistance = it }, "KM")
+        InputField("연비", fuelEconomy, { fuelEconomy = it }, "KM/L")
+        InputField("연료 가격", fuelCost, { fuelCost = it }, "원")
+        InputField("탑승인원", totalPerson, { totalPerson = it }, "명")
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Text("선택:", fontSize = 12.sp)
+        InputField("톨게이트 비용", tollFee, { tollFee = it }, "원", validateBlank = false)
+        InputField("추가 비용(식사, 음료 등)", etcCost, { etcCost = it }, "원", validateBlank = false)
+
         val keyboardController = LocalSoftwareKeyboardController.current
-        Button(onClick = {
-            keyboardController?.hide() // hide keyboard.
-            val distance = totalDistance.toFloatOrNull() ?: 0f
-            val economy = fuelEconomy.toFloatOrNull() ?: 0f
-            val fuel = fuelCost.toIntOrNull() ?: 0
-            val toll = tollFee.toIntOrNull() ?: 0
-            val persons = totalPerson.toIntOrNull() ?: 0
 
-            if (distance > 0 && economy > 0 && fuel > 0 && persons > 0) {
-                val totalFuel = distance / economy
-                val totalCost = (totalFuel * fuel) + toll
-                val costPerPerson = totalCost / persons
+        Spacer(modifier = Modifier.height(8.dp))
 
-                result = """
-                    총 사용 연료: ${"%.2f".format(totalFuel)} 리터
-                    총 비용: ${totalCost.toInt()} 원
-                    1명당 비용: ${costPerPerson.toInt()} 원
-                """.trimIndent()
-            } else {
-                result = "잘못 입력된 필드가 있습니다."
-            }
-        }) {
-            Text("계산")
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            Button(onClick = {
+                // 초기화 버튼 동작
+                totalDistance = ""
+                fuelEconomy = ""
+                fuelCost = ""
+                tollFee = ""
+                etcCost = ""
+                totalPerson = ""
+                result = "None"
+                keyboardController?.hide() // hide keyboard.
+            }) { Text("초기화") }
+
+            Button(onClick = {
+                keyboardController?.hide() // hide keyboard.
+                val distance = totalDistance.toFloatOrNull() ?: 0f
+                val economy = fuelEconomy.toFloatOrNull() ?: 0f
+                val fuel = fuelCost.toIntOrNull() ?: 0
+                val toll = tollFee.toIntOrNull() ?: 0
+                val etc = etcCost.toIntOrNull() ?: 0
+                val persons = totalPerson.toIntOrNull() ?: 0
+
+                if (distance > 0 && economy > 0 && fuel > 0 && persons > 0) {
+                    val totalFuel = distance / economy
+                    val totalCost = (totalFuel * fuel) + toll + etc
+                    val costPerPerson = totalCost / persons
+
+                    result = """
+                총 사용 연료: ${"%.2f".format(totalFuel)} 리터
+                총 비용: ${totalCost.toInt()} 원
+                1명당 비용: ${costPerPerson.toInt()} 원
+            """.trimIndent()
+                } else {
+                    result = "잘못 입력된 필드가 있습니다."
+                }
+            }) { Text("계산") }
+
+
         }
 
         Spacer(modifier = Modifier.height(16.dp))
-        Text("결과:")
+        Text("결과:", fontSize = 12.sp)
         Text(result)
     }
 }
